78adeb8bde39688aa386f5c1af450cba4f21019b
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-02-24 09:17:23 +0000

    added ReportType and reportsExample

--- src/hooks/getReports.ts
+++ src/hooks/getReports.ts
@@ -1,32 +1,33 @@
 import { useState, useEffect } from "react";
+import { reportsExample } from "./reportsExample";
+import type { ReportType } from "../types/ReportType";
 
-export interface useReportsProps {}
-
-export const useReports = ({}: useReportsProps = {}) => {
-  const [reports, setReports] = useState([]);
+export const useReports = () => {
+  const [reports, setReports] = useState<ReportType[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
-  const getReports = async () => {
-    setLoading(true);
+  //   const getReports = async () => {
+  //     setLoading(true);
 
-    try {
-      const res = await fetch(`/api/reports/`);
+  //     try {
+  //       const res = await fetch(`/api/reports/`);
 
-      if (!res.ok) throw new Error(`Failed to fetch reports: ${res.status}`);
+  //       if (!res.ok) throw new Error(`Failed to fetch reports: ${res.status}`);
 
-      const data = await res.json();
-      setReports(data);
-      setError(null);
-    } catch (err: any) {
-      setError(err.message || "Unknown error");
-    } finally {
-      setLoading(false);
-    }
-  };
+  //       const data = await res.json();
+  //       setReports(data);
+  //       setError(null);
+  //     } catch (err: any) {
+  //       setError(err.message || "Unknown error");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
   useEffect(() => {
-    getReports();
+    // getReports();
+    setReports(reportsExample);
   }, []);
 
   return {
---
+++ src/types/ReportType.ts
@@ -0,0 +1,8 @@
+export interface ReportType {
+  _id: string;
+  auditId: string;
+  __v: number;
+  code: string;
+  operator: string;
+  period: string;
+}
