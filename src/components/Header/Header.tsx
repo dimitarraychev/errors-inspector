a107d98a9f1a5999c83edbc0b53278f31b504cca
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-02-24 09:05:35 +0000

    added header, logo and global css

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+import logoSvg from "../../assets/logo.svg";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-wrapper">
+        <img src={logoSvg} alt="Logo" className="logo" />
+        <h1 className="logo-text">API Logs Monitor</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
