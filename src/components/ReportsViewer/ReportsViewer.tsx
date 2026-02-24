a150f39e004b57f4e597d552e1fa2d415beaece9
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-02-24 14:34:08 +0000

    updated reports

--- src/components/ReportsViewer/ReportsViewer.tsx
+++ src/components/ReportsViewer/ReportsViewer.tsx
@@ -1,3 +1,4 @@
+import "./ReportsViewer.css";
 import { useReports } from "../../hooks/useReports";
 import type { ReportItem, ReportOperator } from "../../types/ReportTypes";
 
@@ -7,31 +8,24 @@ const ReportsViewer = () => {
   const sortedReports = [...reports.reports].sort((a, b) => b.total - a.total);
 
   return (
-    <div
-      className="reports-container"
-      style={{ padding: "1rem", fontFamily: "sans-serif" }}
-    >
-      <h2>Reports since: {new Date(reports.since).toLocaleString()}</h2>
+    <div className="reports-container">
+      <h3>Reports since: {new Date(reports.since).toLocaleString()}</h3>
       {sortedReports.map((report: ReportItem) => (
-        <div
-          key={report._id}
-          style={{
-            margin: "1rem 0",
-            border: "1px solid #ccc",
-            borderRadius: 8,
-            padding: 12,
-          }}
-        >
-          <h3>
+        <div className="report-item" key={report._id}>
+          <h4>
             {_idLabel(report._id)} — Total: {report.total}
-          </h3>
+          </h4>
+
           <ul>
-            {report.operators.map((op: ReportOperator) => (
-              <li key={op.operator} style={{ margin: "0.25rem 0" }}>
-                <strong>{op.operator}</strong> — Count: {op.count} — Latest:{" "}
-                {new Date(op.latestPeriod).toLocaleString()}
-              </li>
-            ))}
+            {[...report.operators]
+              .sort((a, b) => b.count - a.count)
+              .map((op: ReportOperator) => (
+                <li className="report-text" key={op.operator}>
+                  <strong>{op.operator}</strong> - Latest:{" "}
+                  {new Date(op.latestPeriod).toLocaleString()} - Total:{" "}
+                  {op.count}
+                </li>
+              ))}
           </ul>
         </div>
       ))}
