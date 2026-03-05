7f7d7be78baff84be196dc399e36a1844105c7dc
Dimitar Raychev <133985222+dimitarraychev@users.noreply.github.com>
2026-03-05 07:43:30 +0000

    updated formatDate

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -6,9 +6,8 @@ export const formatDate = (isoString: string) => {
   const day = String(d.getDate()).padStart(2, "0");
   const hours = String(d.getHours()).padStart(2, "0");
   const minutes = String(d.getMinutes()).padStart(2, "0");
-  const seconds = String(d.getSeconds()).padStart(2, "0");
 
-  return `${hours}:${minutes}:${seconds} ${day}.${month}.${year}`;
+  return `${hours}:${minutes} ${day}.${month}.${year}`;
 };
 
 export const shortFormatDate = (isoString: string, period: string) => {
